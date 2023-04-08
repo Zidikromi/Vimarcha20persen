f0ea553d618bb9e83ecf7e46d7f79d358e74f3c5
Zidikromi <Yazidikromi24@gmail.com>
2023-04-08 17:18:43 +0700

    yeuh bin

--- app/src/main/java/com/example/vimarcha/account.kt
+++ app/src/main/java/com/example/vimarcha/account.kt
@@ -5,55 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.vimarcha.databinding.FragmentAccountBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [account.newInstance] factory method to
- * create an instance of this fragment.
- */
+private lateinit var binding: FragmentAccountBinding
+private lateinit var database: DatabaseReference
 class account : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAccountBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
-    }
+        binding = FragmentAccountBinding.inflate(inflater, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment account.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            account().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            val email = currentUser.email
+            val username = email?.substring(0, email.indexOf("@"))
+            binding.emailtextview.text = username
+        }
+
+        return binding.root
     }
 }
